69ad9e01f3415452a1df2088f9d1bf582a176355
VascoGomesWork <50702151+VascoGomesWork@users.noreply.github.com>
2022-11-27 11:34:27 +0000

    Routes Updated

--- routes/api.php
+++ routes/api.php
@@ -1,6 +1,10 @@
 <?php
 
+use App\Http\Controllers\ComentariosEventosController;
+use App\Http\Controllers\EventosController;
+use App\Http\Controllers\OrganizadorController;
 use App\Http\Controllers\ParticipanteController;
+use App\Http\Controllers\ParticipanteInscreveEventosController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -30,4 +34,18 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
 
 Route::post('/participante/store', [ParticipanteController::class, 'store']);
 Route::get('/participante/show/{id}', [ParticipanteController::class, 'show']);
-//Route::get('/clienteinscreveevento/{post}', [\App\Http\Controllers\ClienteInscreveEventoController::class, 'show']);*/
+
+Route::get('/organizador/store', [OrganizadorController::class, 'show']);
+Route::get('/organizador/show/{id}', [OrganizadorController::class, 'show']);
+
+Route::get('/eventos', [EventosController::class, 'index']);
+Route::get('/eventos/store', [EventosController::class, 'store']);
+Route::get('/eventos/show/{id}', [EventosController::class, 'show']);
+
+Route::get('/inscrever_eventos', [ParticipanteInscreveEventosController::class, 'index']);
+Route::get('/inscrever_eventos/store', [ParticipanteInscreveEventosController::class, 'store']);
+Route::get('/inscrever_eventos/show/{id}', [ParticipanteInscreveEventosController::class, 'show']);
+
+Route::get('/comentarios_evento', [ComentariosEventosController::class, 'index']);
+Route::get('/comentarios_evento/store', [ComentariosEventosController::class, 'store']);
+Route::get('/comentarios_evento/show/{id}', [ComentariosEventosController::class, 'show']);
